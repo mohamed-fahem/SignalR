a577c05ea59daf028b494724b7483d5567be9665
mohamed <mohamedtest@gmail.com>
2023-11-30 14:18:33 +0200

    Add project files.

---
+++ SignalR/wwwroot/js/site.js
@@ -0,0 +1,17 @@
+﻿var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+
+connection.on("ReceiveMessage", function (fromUser, message) {
+    var msg = fromUser + ": " + message;
+    var li = document.createElement("li");
+    li.textContent = msg;
+    $("#List").prepend(li);
+});
+
+connection.start();
+
+$("#btnSend").on("click", function () {
+    var fromUser = $("#txtUser").val();
+    var messege = $("#txtMessage").val();
+
+    connection.invoke("SendMessage",fromUser,messege);
+});
